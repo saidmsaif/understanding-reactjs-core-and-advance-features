8b9410943b0d405df126c2aa51623b3c52933dc9
saidmsaif <saidmsaiff@gmail.com>
2021-08-20 23:11:39 +0600

    initialize new app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class componentName extends Component {
+export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>This</h1>
+                <h1>Saifullah</h1>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './components/Index';
+import App from './components/App';
 import './css/tailwind.css'
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Index />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
