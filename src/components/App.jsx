42aafdbbd32e97c4d120b7486ec685b6b21160af
saidmsaif <saidmsaiff@gmail.com>
2021-08-20 23:14:58 +0600

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Saifullah</h1>
+                <h1>Saifullah Saif</h1>
             </div>
         )
     }
